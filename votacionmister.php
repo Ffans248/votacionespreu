f4c0e9e327efdc87364fee23126ac7c635c0427d
unknown <javbausotoros@gmail.com>
2023-07-15 16:00:14 -0600

    Rama

--- votacionmister.php
+++ votacionmister.php
@@ -2,7 +2,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
+    <title>A</title>
     <!-- Google Fonts Link -->
     <link
       href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700;800&display=swap"
