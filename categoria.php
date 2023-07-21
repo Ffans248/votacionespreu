89373080a2e9389fe39ec69a8a8e03c8d301539e
KrSAdal <adal.ac.ramirez@gmail.com>
2023-07-20 20:46:48 -0600

    Se hizo la categoría de elección, miss o mister

--- categoria.php
+++ categoria.php
@@ -1,60 +1,47 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-  <!-- Google Fonts Link -->
-  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700;800&display=swap"
-    rel="stylesheet" />
-  <!-- Line Awesome CDN Link -->
-  <link rel="stylesheet" href="styles.css">
-  <link rel="stylesheet"
-    href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <!-- Google Fonts Link -->
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;500;700;800&display=swap"
+        rel="stylesheet" />
+    <!-- Line Awesome CDN Link -->
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet"
+        href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
 <body>
-  <div class="main-container">
-    <h2>What's your favourite hobbie?</h2>
-    <div class="radio-buttons">
-      <label class="custom-radio">
-        <input type="radio" name="radio" checked />
-        <span class="radio-btn"><i class="las la-check"></i>
-          <div class="hobbies-icon">
-            <i class="las la-biking"></i>
-            <h3>UWU</h3>
-          </div>
-        </span>
-      </label>
-      <label class="custom-radio">
-        <input type="radio" name="radio" />
-        <span class="radio-btn"><i class="las la-check"></i>
-          <div class="hobbies-icon">
-            <i class="las la-futbol"></i>
-            <h3>Football</h3>
-          </div>
-        </span>
-      </label>
-      <label class="custom-radio">
-        <input type="radio" name="radio" />
-        <span class="radio-btn"><i class="las la-check"></i>
-          <div class="hobbies-icon">
-            <i class="las la-table-tennis"></i>
-            <h3>Table Tennis</h3>
-          </div>
-        </span>
-      </label>
-      <label class="custom-radio">
-        <input type="radio" name="radio" />
-        <span class="radio-btn"><i class="las la-check"></i>
-          <div class="hobbies-icon">
-            <i class="las la-ellipsis-h"></i>
-            <h3>Other</h3>
-          </div>
-        </span>
-      </label>
+    <div class="main-container">
+        <h2>Elige Categoría</h2>
+        <div class="radio-buttons">
+            <a href="votacionmister.php"><label class="custom-radio">
+                    <input type="submit" name="radio" checked />
+                    <span class="radio-btn"><i class="las la-check"></i>
+                        <div class="hobbies-icon">
+                            <i class="bi bi-gender-male"></i><br><br>
+                            <h3>Masculino</h3>
+
+                        </div>
+                    </span>
+                </label>
+            </a>
+            <a href="votacionmiss.php"><label class="custom-radio">
+                    <input type="submit" name="radio" />
+                    <span class="radio-btn"><i class="las la-check"></i>
+                        <div class="hobbies-icon">
+                            <i class="bi bi-gender-female"></i><br><br>
+                            <h3>Femenino</h3>
+
+                        </div>
+                    </span>
+                </label>
+            </a>
+        </div>
     </div>
-  </div>
 </body>
 
 </html>
