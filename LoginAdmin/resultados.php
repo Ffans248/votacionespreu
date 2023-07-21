41c8b4e7ec75410af2e2590efc0cb0d927d20208
unknown <javbausotoros@gmail.com>
2023-07-21 01:59:05 -0600

    Gráfica

--- LoginAdmin/resultados.php
+++ LoginAdmin/resultados.php
@@ -3,16 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gráficas de Votación</title>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <title>Gráficas Miss y Mister PREU</title>
 </head>
 <body>
-    <div>
-        <canvas id="chartMiss" width="400" height="400"></canvas>
-    </div>
-    <div>
-        <canvas id="chartMister" width="400" height="400"></canvas>
-    </div>
-    <script src="main.js"></script>
+<canvas id="myChart" width="400" height="400"></canvas>
+
+<script>
+    const ctx = document.getElementById('myChart');
+
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+</script>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </body>
 </html>
