df22c54c7f9854c5c186d9ea3ef8fef3f01d4478
KrSAdal <adal.ac.ramirez@gmail.com>
2023-07-21 02:28:53 -0600

    se añadieron verificaciones de sesión en votomiss.php y votomister.php

--- votomiss.php
+++ votomiss.php
@@ -1,38 +1,45 @@
-<?php 
+<?php
 session_start();
-$id=$_SESSION['id'];
-$cod=$_SESSION['codigo_sica'];
+include_once('conexion.php');
 
-include('conexion.php');
-if (!$conexion) {
-    die("La conexión fallo: " . mysqli_connect_error());
-} else {
+if (isset($_SESSION['id']) && isset($_SESSION['codigo_sica'])) {
+    $id = $_SESSION['id'];
+    $cod = $_SESSION['codigo_sica'];
+
+    include('conexion.php');
+    if (!$conexion) {
+        die("La conexión fallo: " . mysqli_connect_error());
+    } else {
         if (empty($_POST["radio"])) {
             header('Location: votacionmiss.php?error=<script>alert("¡No has seleccionado una candidata!");</script>');
-        }else {
-    $idmiss = $_POST['radio'];
-    $sql = mysqli_query($conexion,"SELECT * FROM votos WHERE fk_alumno = '$id'");
-    $r = mysqli_num_rows($sql);
-    if ($r > 0) {
-        
-        
-       
-        $sql = "UPDATE votos SET miss=$idmiss, estado_miss=1";
-        
-    } else {
-        
-        $sql = "INSERT INTO votos (fk_alumno, miss, estado_miss) VALUES ($id, $idmiss, 1)";
+        } else {
+            $idmiss = $_POST['radio'];
+            $sql = mysqli_query($conexion, "SELECT * FROM votos WHERE fk_alumno = '$id'");
+            $r = mysqli_num_rows($sql);
+            if ($r > 0) {
 
-    if (mysqli_query($conexion, $sql)) {
-        header("Location: index.php");
-    } else {
-        echo "Error: " . mysqli_error($conexion);
-    }
-    }
-    
 
+
+                $sql = "UPDATE votos SET miss=$idmiss, estado_miss=1";
+
+            } else {
+
+                $sql = "INSERT INTO votos (fk_alumno, miss, estado_miss) VALUES ($id, $idmiss, 1)";
+
+                if (mysqli_query($conexion, $sql)) {
+                    header("Location: index.php");
+                } else {
+                    echo "Error: " . mysqli_error($conexion);
+                }
+            }
+
+
+        }
+
+
+    }
+} else {
+    header('location: index.php');
+    exit();
 }
-
-        
-    }?>
-
+?>
